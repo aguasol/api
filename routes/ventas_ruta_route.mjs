a26e99e90f7e79c321cccd8889e2290e35d0baae
Luchin151090 <lgonzalesu@ulasalle.edu.pe>
2024-03-09 10:29:25 -0500

    sdfsdfs

--- routes/ventas_ruta_route.mjs
+++ routes/ventas_ruta_route.mjs
@@ -1,7 +1,8 @@
-import { createRutas, getLastRutas,getLastRutasConductor } from "../controllers/ventas_ruta_controller.mjs"; 
+import { createRutas, getLastRutas,getLastRutasConductor,getPedidosByrutas } from "../controllers/ventas_ruta_controller.mjs"; 
 import express from 'express';
 const routerVentasRuta = express.Router();
 routerVentasRuta.post('/ruta',createRutas)
+routerVentasRuta.get('/ruta/:empleadoId',getPedidosByrutas)
 routerVentasRuta.get('/rutalast/:empleadoId',getLastRutas)
 routerVentasRuta.get('/rutakastcond/:conductorId',getLastRutasConductor)
 
