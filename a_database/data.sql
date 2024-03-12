cebce26290e57aa9091698cf47b16b47a2b2eecd
Luchin151090 <lgonzalesu@ulasalle.edu.pe>
2024-03-12 13:34:01 -0500

    HJYUYUJYUJ

--- a_database/data.sql
+++ a_database/data.sql
@@ -6,25 +6,18 @@ INSERT INTO relaciones.roles (nombre) VALUES
   ('cliente'),
   ('conductor');
     -- Insertar registros en la tabla zona_trabajo
-INSERT INTO ventas.zona_trabajo (nombre) VALUES
+INSERT INTO ventas.zona_trabajo (nombre,poligono,departamento,provincia) VALUES
+('erick','-16,451525-71,563376,-16,4220885-71,5915761,-16,394172-71,585325,-16,385613-71,581683,-16,368901-71,578649,-16,348927-71,582169,-16,332329-71,591515,-16,308506-71,557315,-16,318757-71,531332,-16,339615-71,511454,-16,379966-71,499662,-16,38396-71,505928,-16,394499-71,518459,-16,393841-71,535061,-16,399646-71,540811,-16,402239-71,536992,-16,40298-71,532743,-16,407015-71,525276,-16,410406-71,50917,-16,423592-71,509746','arequipa','arequipa'),
   ('camana'),
-  ('la joya'),
+  ('la joya','-16.410194,-71.847502,-16.428912,-71.862066,-16.496397,-71.865025,-16.534348,-71.956033,-16.623462,-71.958461,-16.638349,-71.906509,-16.604851,-71.833679,-16.542493,-71.794351,-16.535112,-71.761402,-16.510844,-71.786307,-16.501993,-71.805498,-16.483861,-71.810983,-16.455484,-71.814159,-16.439286,-71.793373,-16.417548,-71.797992,-16.407025,-71.830326','arequipa','arequipa'),
   ('omate');
   
  -- Insertar registros en la tabla usuario
 INSERT INTO personal.usuario (rol_id,nickname, contrasena, email) VALUES
   (1,'coquin', 'contrasena1', 'usuario1@example.com'),
   (2,'pepito', 'contrasena2', 'usuario2@example.com'),
-  (2,'anita', 'contrasena2', 'usuario2@example.com'),
   (3,'ramon', 'contrasena2', 'usuario2@example.com'),
-  (4,'pablito', 'contrasena2', 'usuario2@example.com'),
-  (4,'luchin', 'contrasena2', 'usuario2@example.com'),
-  (4,'joprgito', 'contrasena2', 'usuario2@example.com'),
-  (4,'julita', 'contrasena2', 'usuario2@example.com'),
   (4,'pepin', 'contrasena2', 'usuario2@example.com'),
-  (4,'cris', 'contrasena2', 'usuario2@example.com'),
-  (5,'lucas', 'contrasena3', 'usuario3@example.com'),
-  (5,'panchito', 'contrasena3', 'usuario3@example.com'),
   (5,'robert', 'contrasena3', 'usuario3@example.com');
   
   
@@ -53,12 +46,6 @@ INSERT INTO personal.conductor (usuario_id, nombres, apellidos, licencia, dni, f
   (11, 'lucrecia', 'Apellido3', 'DEF456', '7890123456', '1980-12-04',1),
   (12, 'panchita camionera', 'Apellido3', 'DEM456', '23453234', '1990-12-04',1),
   (13, 'robertoto', 'sanchez', 'DEM456', '23453234', '1990-12-04',1);
-
-
-	
-
-  
-
 	
 	
   -- Insertar registros en la tabla cliente
@@ -100,16 +87,18 @@ INSERT INTO ventas.pedido (ruta_id, cliente_nr_id, subtotal,descuento,total, fec
   
   -- Insertar registros en la tabla producto
 INSERT INTO ventas.producto (nombre, precio, descripcion,foto) VALUES
-  ('botella 700ml',10.00,'paquete x 15 und.','BIDON0.png'),
-  ('botella 3l',9.00,'paquete x 9 und.','BIDON03.png'),
-  ('botella 7l',5.50,'und.','BIDON7.png'),
-  ('bidon 20l',20.00,'und.','BIDON20.png'),
-  ('recarga',10.00,'und.','RECARGA.png');
+('recarga',13.00,'und.','RECARGA.png');
+('bidon 20l',35.00,'und.','BIDON20.png'),
+('botella 7l',6.00,'und.','BIDON7.png'),
+('botella 3l',9.00,'pqt. 4 und. (S/.2.25 c/u)','BIDON03.png'),
+  ('botella 700ml',12.00,'pqt. x 15 und. (S/.0.80 c/u)','BIDON0.png'),
+  ('bidon vacio',0.00,'und.','RECARGA.png');
 
 INSERT INTO ventas.promocion (nombre, precio, descripcion, fecha_inicio,fecha_limite,foto) VALUES
-  ('Promocion Veraniega',48.00,'Lleva 2 bidones NUEVOS x S/. 48.00',current_timestamp,current_timestamp,'BIDON0.png'),
-  ('Promociòn de Locura',27.00,'Compra 5 bidones de 7L y lleva 1 GRATIS',current_timestamp,current_timestamp,'BIDON03.png'),
-  ('Paquete Playero',39.50,'Por S/.39.50 lleva 1 bidón nuevo y 3 botellas de 3L',current_timestamp,current_timestamp,'RECARGA.png');
+  ('Promocion regalona',13.00,'Lleva una recarga y recibe un Vasito Sol de REGALO',current_timestamp,current_timestamp,'BIDON20.png'),
+  ('Promoción de locura',60.00,'Compra 10 bidones de 7L y lleva 1 GRATIS',current_timestamp,current_timestamp,'BIDON7.png'),
+  ('Promoción de verano',16.00,'Lleva 2 botelas de 3L a solo S/.16.00. AHORRAS S/. 2.00',current_timestamp,current_timestamp,'BIDON03.png'),
+  ('Promoción personales',20.00,'Lleva 2 paquetes de 700ml a solo S/.20.00. AHORRAS S/.4.00',current_timestamp,current_timestamp,'BIDON0.png');
 
 INSERT INTO relaciones.producto_promocion (promocion_id, producto_id, cantidad) VALUES
   (1,4,2),
