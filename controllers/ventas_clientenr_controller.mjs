bf83ebb3e12d4e9022a1f14ca070b0bc5c76f0df
Luchin151090 <lgonzalesu@ulasalle.edu.pe>
2024-03-12 11:21:34 -0500

    final error ?

--- controllers/ventas_clientenr_controller.mjs
+++ controllers/ventas_clientenr_controller.mjs
@@ -4,7 +4,7 @@ export const createClientesnr = async (req,res) => {
     try {
         const newcliente = req.body
         const createcliente= await modelClientenr.createClientenr(newcliente);
-        res.json(createcliente);
+        res.status(200).json(createcliente);
     } catch (error) {
         res.status(500).json({error:error.message});
 
@@ -15,8 +15,8 @@ export const getClienteNR =  async (req,res) => {
     try {
         const getCliente = await modelClientenr.getClientenr();
         //console.log("----controller pedido")
-       // console.log(getPedidos)
-        res.json(getCliente)
+       // consolelog(getPedidos)
+        res.status(200).json(getCliente)
     } catch (error) {
         res.status(500).json({erro:error.message})
     }
