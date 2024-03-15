bdb6860d24c05e8f6ca810179cfed766998d9f96
Luchin151090 <lgonzalesu@ulasalle.edu.pe>
2024-03-15 09:30:39 -0500

    h

--- controllers/ventas_ruta_controller.mjs
+++ controllers/ventas_ruta_controller.mjs
@@ -5,7 +5,7 @@ export const createRutas = async (req,res) => {
         const newRuta = req.body
         const rutaCreada= await modelRuta.createRuta(newRuta);
         
-        res.json(rutaCreada);
+        res.status(200).json(rutaCreada);
     } catch (error) {
         res.status(500).json({error:error.message});
 
@@ -16,9 +16,9 @@ export const getLastRutas =  async (req,res) => {
         const { empleadoId} = req.params;
         const empleado_id = parseInt(empleadoId,10);
         const getLast =  await modelRuta.getLastRuta(empleado_id)
-        res.json(getLast);
+        res.status(200).json(getLast);
     } catch (error) {
-        res.status(500).json({"message":"NO DATA"})
+        res.status(500).json({error:error.message});
     }
 }
 
@@ -27,9 +27,10 @@ export const getPedidosByrutas = async (req,res) => {
         const { rutaId} = req.params;
         const id = parseInt(rutaId,10);
         const getPedidos =  await modelRuta.getPedidosByruta(id)
-        res.json(getPedidos);
+        res.status(200).json(getPedidos);
     } catch (error) {
-        
+        res.status(500).json({error:error.message});
+
     }
 }
 export const getLastRutasConductor =  async (req,res) => {
@@ -37,8 +38,8 @@ export const getLastRutasConductor =  async (req,res) => {
         const { conductorId} = req.params;
         const conductor_id = parseInt(conductorId,10);
         const getLast =  await modelRuta.getLastRutaConductor(conductor_id)
-        res.json(getLast);
+        res.status(200).json(getLast);
     } catch (error) {
-        res.status(500).json({"message":"NO DATA"})
+        res.status(500).json({error:error.message});
     }
 }
