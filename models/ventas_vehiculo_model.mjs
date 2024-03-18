059d13f4e3d2843d64a17e2bf7840fc77800f64e
Luchin151090 <lgonzalesu@ulasalle.edu.pe>
2024-03-18 12:21:01 -0500

    database

--- models/ventas_vehiculo_model.mjs
+++ models/ventas_vehiculo_model.mjs
@@ -6,40 +6,38 @@ const modelVehiculo = {
 
     createVehiculo: async (vehiculo) => {
         try {
-            console.log("vehiculo")
-            console.log(vehiculo)
+            console.log("vehiculo");
+            console.log(vehiculo);
             const result = await db_pool.one(`
-            INSERT INTO ventas.vehiculo 
-            (nombre_modelo,placa,administrador_id)
-             VALUES($1,$2,$3) RETURNING *`,
-                [vehiculo.nombre_modelo, vehiculo.placa, vehiculo.administrador_id])
+                INSERT INTO ventas.vehiculo 
+                (nombre_modelo, placa, administrador_id)
+                VALUES ($1, $2, $3)
+                RETURNING *`,
+                [vehiculo.nombre_modelo, vehiculo.placa, vehiculo.administrador_id]);
 
             const vehiculo_productos = await db_pool.manyOrNone(`
-            INSERT INTO ventas.vehiculo_producto
-            (producto_id,
-                vehiculo_id,
-                stock,
-                stock_movil_conductor,
-                stock_movil_empleado)
-            VALUES 
-            ($1,$2,$3,$4,$5),
-            ($1,$2,$3,$4,$5),
-            ($1,$2,$3,$4,$5),
-            ($1,$2,$3,$4,$5),
-            ($1,$2,$3,$4,$5)
-            RETURNING *`,
-            [[1,result.id,0,0,0],
-            [2,result.id,0,0,0],
-            [3,result.id,0,0,0],
-            [4,result.id,0,0,0],
-            [5,result.id,0,0,0],
-        ])
-            console.log("salida creacion")
-            console.log(vehiculo)
-            return result
+                INSERT INTO ventas.vehiculo_producto
+                (producto_id, vehiculo_id, stock, stock_movil_conductor, stock_movil_empleado)
+                VALUES 
+                ($1, $2, $3, $4, $5),
+                ($6, $7, $8, $9, $10),
+                ($11, $12, $13, $14, $15),
+                ($16, $17, $18, $19, $20),
+                ($21, $22, $23, $24, $25)
+                RETURNING *`,
+                [
+                    1, result.id, 0, 0, 0,
+                    2, result.id, 0, 0, 0,
+                    3, result.id, 0, 0, 0,
+                    4, result.id, 0, 0, 0,
+                    5, result.id, 0, 0, 0
+                ]);
 
+            console.log("salida creacion");
+            console.log(vehiculo);
+            return result;
         } catch (error) {
-            throw new Error(`error query ${error}`)
+            throw new Error(`error query ${error}`);
         }
     },
     getVehiculo: async (id) => {
