21dcb36927fb47a67e73e5c3843263bb3595998a
SaraSanchezA <151571780+SaraSanchezA@users.noreply.github.com>
2024-03-21 09:52:00 -0500

    Update usuario_cliente_model.mjs

--- models/usuario_cliente_model.mjs
+++ models/usuario_cliente_model.mjs
@@ -81,7 +81,7 @@ const modelUserCliente = {
     },
     updateCliente: async (id, cliente) => {
         try {
-            const cliente1 = await db_pool.one('UPDATE ventas.cliente SET saldo_beneficios=$1, suscripcion=$2, frecuencia=$3, quiereretirar=$4, medio_retiro=$5, banco_retiro=$6, numero_cuenta=$7 WHERE usuario_id = $8 RETURNING *',
+            const cliente1 = await db_pool.one('UPDATE ventas.cliente SET saldo_beneficios=$1, suscripcion=$2, frecuencia=$3, quiereretirar=$4, medio_retiro=$5, banco_retiro=$6, numero_cuenta=$7 WHERE id = $8 RETURNING *',
                 [cliente.saldo_beneficios, cliente.suscripcion, cliente.frecuencia, cliente.quiereretirar, cliente.medio_retiro, cliente.banco_retiro, cliente.numero_cuenta, id]);
             console.log("dentro de model 2do update", id)
             return { cliente1 }
