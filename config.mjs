3e8b4b92cc35aa579a43eefe70495dedc12e7991
Luchin151090 <lgonzalesu@ulasalle.edu.pe>
2024-07-30 21:25:23 -0500

    nuevabase de datos

--- config.mjs
+++ config.mjs
@@ -2,10 +2,10 @@ import pgPromise from 'pg-promise';
 
 const pgp = pgPromise();
 
-//const connectionStr = "postgres://postgres:1234@localhost:5432/newsol2024";
+const connectionStr = "postgresql://aguasol_xar2_user:WCJ9WKpeqQdIirMX1z85mRvDsB27IhX5@dpg-cqkps2ogph6c738j8r90-a.oregon-postgres.render.com/aguasol_xar2";
 //const connectionStr = "postgresql://aguasol:TntaHgQf9msnfmHXdrQWEXHEt1hut1MC@dpg-cml86oacn0vc739oj51g-a.oregon-postgres.render.com/aguasol_ui5l";
 //const connectionStr = "postgres://aguasol:TntaHgQf9msnfmHXdrQWEXHEt1hut1MC@dpg-cml86oacn0vc739oj51g-a/aguasol_ui5l";
-const connectionStr = "postgres://aguasol:TntaHgQf9msnfmHXdrQWEXHEt1hut1MC@dpg-cml86oacn0vc739oj51g-a.oregon-postgres.render.com/aguasol_ui5l";
+//const connectionStr = "postgres://aguasol:TntaHgQf9msnfmHXdrQWEXHEt1hut1MC@dpg-cml86oacn0vc739oj51g-a.oregon-postgres.render.com/aguasol_ui5l";
 export const db_pool = pgp({
     connectionString: connectionStr,
     ssl: {
